e8b4ead0adac44e3fb7abdc46acbdfcd53edb1fe
ALPRO_A_23-191_AHMAD MUSHOFA <143861065+Ahmad-Mushofa@users.noreply.github.com>
2025-05-23 00:04:23 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel session
-session_destroy(); // Menghancurkan session
-
-// Redirect ke halaman login
-header("Location: login.php");
-exit();
